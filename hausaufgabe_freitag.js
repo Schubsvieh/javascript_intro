97600aaaaab133fb6efb581e9268446dd5d0fb1f
Schubsvieh <franziskakuehne55.com>
2024-03-15 15:18:16 +0100

    zwischenstand

---
+++ hausaufgabe_freitag.js
@@ -0,0 +1,19 @@
+let lieblingsbuch = "Alex Verus";
+
+let anzahlGeschwister = 2;
+
+let hatFuehrerschein = false;
+
+let momentaneStimmung = null;
+
+let naechstesReiseziel;
+
+console.log("1. Lieblingsbuch:", lieblingsbuch, typeof lieblingsbuch);
+
+console.log("2. Anzahl Geschwister:", anzahlGeschwister, typeof anzahlGeschwister);
+
+console.log("3. hat einen Führerschein:", hatFuehrerschein, typeof hatFuehrerschein);
+
+console.log("4. Meine momentane Stimmung:", momentaneStimmung, typeof momentaneStimmung);
+
+console.log("5. Mein nächstes Reiseziel:", naechstesReiseziel, typeof naechstesReiseziel);
