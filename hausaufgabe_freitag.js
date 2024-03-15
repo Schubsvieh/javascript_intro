8a96b0a8cb0d3793d2355385f8a719bd0fd8b526
Schubsvieh <franziskakuehne55.com>
2024-03-15 16:23:38 +0100

    finally

--- hausaufgabe_freitag.js
+++ hausaufgabe_freitag.js
@@ -8,6 +8,38 @@ let momentaneStimmung = null;
 
 let naechstesReiseziel;
 
+let lieblingsFarbe = "schwarz";
+
+let wohnflaeche = 62;
+
+let magKaffee = true;
+
+let aktuellesProjekt = null;
+
+let letztesAbendessen;
+
+let geburtstadt = "Bernau";
+
+let kaffeetassenProTag = 5;
+
+let istVegetarier = false;
+
+let zukunftsWunsch = null;
+
+let reiten;
+
+let lieblingsFilm = "Top Gun Maverick";
+
+let hoeheincm = 160;
+
+let hatHaustier = true;
+
+let Mittagessen = null;
+
+let LieblingsMusikBand;
+
+
+
 console.log("1. Lieblingsbuch:", lieblingsbuch, typeof lieblingsbuch);
 
 console.log("2. Anzahl Geschwister:", anzahlGeschwister, typeof anzahlGeschwister);
@@ -17,3 +49,36 @@ console.log("3. hat einen Führerschein:", hatFuehrerschein, typeof hatFuehrersc
 console.log("4. Meine momentane Stimmung:", momentaneStimmung, typeof momentaneStimmung);
 
 console.log("5. Mein nächstes Reiseziel:", naechstesReiseziel, typeof naechstesReiseziel);
+
+console.log("6. Lieblingsfarbe:", lieblingsFarbe, typeof lieblingsFarbe);
+
+console.log("7. Wohnfläche:", wohnflaeche, typeof wohnflaeche);
+
+console.log("8. mag Kaffee:", magKaffee, typeof magKaffee);
+
+console.log("9. aktuelles Projekt:", aktuellesProjekt, typeof aktuellesProjekt);
+
+console.log("10. letztes Abendessen:", letztesAbendessen, typeof letztesAbendessen);
+
+console.log("11. Geburtstadt:", geburtstadt, typeof geburtstadt);
+
+console.log("12. Anzahl Kaffeetassen pro Tag:", kaffeetassenProTag, typeof kaffeetassenProTag);
+
+console.log("13. Bin Vegetarier:", istVegetarier, typeof istVegetarier);
+
+console.log("14. Mein Zukunftswunsch:", zukunftsWunsch, typeof zukunftsWunsch);
+
+console.log("15. Lieblingssportart:", reiten, typeof reiten);
+
+console.log("16. Lieblingsfilm:", lieblingsFilm, typeof lieblingsFilm);
+
+console.log("17. Größe in cm:", hoeheincm, typeof hoeheincm);
+
+console.log("18. Hat Haustiere:", hatHaustier, typeof hatHaustier);
+
+console.log("19. Mittagessen:", Mittagessen, typeof Mittagessen);
+
+console.log("20. Lieblinsband:", LieblingsMusikBand, typeof LieblingsMusikBand);
+
+
+
